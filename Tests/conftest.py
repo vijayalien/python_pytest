50820e9ef40da2bcf1d1160807bfae09645b166a
vijay <vijaysalian94@gmail.com>
2022-03-28 22:37:10 +0530

    gitignore and minor changes

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -6,14 +6,14 @@ from webdriver_manager.firefox import GeckoDriverManager
 from Config.config import TestData
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope='class', params=["chrome"])
 def init_driver(request):
-    # if request.params == "chrome":
-    #     web_driver = webdriver.Chrome(ChromeDriverManager().install())
-    # if request.params == "firefox":
-    #     web_driver = webdriver.Firefox(GeckoDriverManager().install())
-    # request.cls.driver = web_driver
-    web_driver = webdriver.Chrome(executable_path=TestData.EXECUTABLE_PATH)
+    if request.param == "chrome":
+        web_driver = webdriver.Chrome(executable_path=TestData.EXECUTABLE_PATH)
+    if request.param == "firefox":
+        web_driver = webdriver.Firefox(GeckoDriverManager().install())
+    request.cls.driver = web_driver
+    # web_driver = webdriver.Chrome(executable_path=TestData.EXECUTABLE_PATH)
     web_driver.maximize_window()
     request.cls.driver = web_driver
     yield
